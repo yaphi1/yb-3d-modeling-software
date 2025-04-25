9a9135c917865b5cd6e7b411a1ed3192ecc3529e
Yaphi Berhanu <yaphi1@gmail.com>
2025-04-25 00:01:35 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+To run this app:
+
+```
+npm start
+```
---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="Hello">
+      <div>Hi</div>
+      <img width="200" alt="icon" src={icon} />
+    </div>
+  );
+}
