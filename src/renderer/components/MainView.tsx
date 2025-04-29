7f659558167f4a89bbd1c599886ced2044f6049d
Yaphi Berhanu <yaphi1@gmail.com>
2025-04-28 23:55:35 -0400

    Add sky to material preview mode

--- src/renderer/components/MainView.tsx
+++ src/renderer/components/MainView.tsx
@@ -1,12 +1,16 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sky } from '@react-three/drei';
 import { useContext } from 'react';
 import { EditorGrid } from './EditorGrid';
 import { Lighting } from './Lighting';
 import { SceneObjects } from './SceneObjects';
 import { useSelectionHelpers } from '../useSelectionHelpers';
 import { useMouseMoveHandler } from './controls/useMouseMoveHandler';
-import { EDITING_STATES, EditorContext } from './contexts/EditorContext';
+import {
+  EDITING_STATES,
+  EditorContext,
+  VIEWING_MODES,
+} from './contexts/EditorContext';
 import { Axes } from './Axes';
 import { useEditorStateHelpers } from '../useEditorStateHelpers';
 
@@ -17,6 +21,8 @@ export function MainView() {
   const { setEditingStateToDefault } = useEditorStateHelpers();
 
   const isMoving = editorState.editingState === EDITING_STATES.MOVE;
+  const isMaterialPreview =
+    editorState.viewingMode === VIEWING_MODES.MATERIAL_PREVIEW;
 
   return (
     <Canvas
@@ -33,6 +39,7 @@ export function MainView() {
       }}
     >
       <Lighting />
+      {isMaterialPreview && <Sky sunPosition={1} />}
       <SceneObjects />
       <EditorGrid />
       <Axes />
